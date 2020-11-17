cde646581ce8b6c47955a3df37560a570aae3340
Josh K Preston <kanemetal@gmail.com>
2020-11-17 14:57:21 -0700

    pluralize explicit

--- yardsalelocator.server/server/db/DbConfig.js
+++ yardsalelocator.server/server/db/DbConfig.js
@@ -5,6 +5,7 @@ mongoose.set('useNewUrlParser', true)
 mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
 mongoose.set('useUnifiedTopology', true)
+mongoose.pluralize(null)
 mongoose.connection.on('error', err => {
   logger.error('[DATABASE ERROR]:', err)
 })
