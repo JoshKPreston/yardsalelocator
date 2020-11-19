f47ce6f69daee86a61ea46b78ecbe790894f11db
briglowell <brig.lowell@gmail.com>
2020-11-19 10:01:59 -0700

    tags model added

---
+++ yardsalelocator.client/src/assets/models/Tag.js
@@ -0,0 +1,6 @@
+
+const Tag {
+  defaultTags = ['baby', 'kids', 'clothing', 'tools', 'electronics', 'games', 'furniture', 'appliances', 'computers', 'vehicles', 'kitchen', 'free', 'antiques', 'firearms']
+}
+
+export default Tag
