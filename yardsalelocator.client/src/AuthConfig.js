6136bb9f455672837fe6e094e16716bbfabf2a50
bristonlowell@gmail.com <bristonlowell@gmail.com>
2022-12-21 13:03:04 -0500

    change to auth config

--- yardsalelocator.client/src/AuthConfig.js
+++ yardsalelocator.client/src/AuthConfig.js
@@ -1,6 +1,17 @@
-export const domain = 'bcwclassroom.us.auth0.com'
-export const clientId = 'hJgtcn911QsufT20bCh8ChHMtvuoFqd8'
-export const audience = 'https://yardsalelocator/api'
+// export const domain = 'bcwclassroom.us.auth0.com'
+// export const clientId = 'hJgtcn911QsufT20bCh8ChHMtvuoFqd8'
+// export const audience = 'https://yardsalelocator/api'
+// export const authHeader = 'prj_test_pk_84c02a43b10a046d097af851761c145f54316090'
+// // export const MAP_API_KEY = 'AIzaSyA3d9hA6pbLww_QEaABa94ZqMpGs_Anmrc'
+// export const MAP_API_KEY = 'AIzaSyDMFGolYKFoFHm_jTvjshRmeHYjqbJvo-Q'
+
+// export const MAP_CALLBACK_NAME = 'initMap'
+
+export const domain = 'dev-kch8o0rp.us.auth0.com'
+export const clientId = 'OiX7R1te2uGfd4oerwms6pxOf607u7Ht'
+export const audience = 'https://yard-sale-time'
 export const authHeader = 'prj_test_pk_84c02a43b10a046d097af851761c145f54316090'
-export const MAP_API_KEY = 'AIzaSyA3d9hA6pbLww_QEaABa94ZqMpGs_Anmrc'
+// export const MAP_API_KEY = 'AIzaSyA3d9hA6pbLww_QEaABa94ZqMpGs_Anmrc'
+export const MAP_API_KEY = 'AIzaSyDMFGolYKFoFHm_jTvjshRmeHYjqbJvo-Q'
+
 export const MAP_CALLBACK_NAME = 'initMap'
